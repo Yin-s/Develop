307074c08823cf8bb6952a3b635ea09636de6c70
zkk97 <zkk970907@163.com>
2020-10-10 17:09:53 +0800

    赵可可 划分区间

--- StudyJava/src/main/SecClass.java
+++ StudyJava/src/main/SecClass.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.ArrayList;
+
 public class SecClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		int a,b,c=0;//a是0-40，b是40-80，c是80-100
+		public void Section(ArrayList arr){
+			
+			for(int i=0;i<arr.size();i++){
+				if((int)arr.get(i)>0 && (int)arr.get(i) < 40)
+					a++;
+				else if((int)arr.get(i)>=40 && (int)arr.get(i) < 80)
+					b++;
+				else if((int)arr.get(i)>=80 && (int)arr.get(i) < 100)
+					c++;
+			}
+			System.out.println("0-40区间的有"+a+"个，40-80区间的有"+b+"个，80-100区间的有"+c+"个");
+		}
 
-	}
-
+	
 }
