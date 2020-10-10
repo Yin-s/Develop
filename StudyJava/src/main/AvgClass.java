1f4db3f9a09053d84ff31e266c85f5dd82e7722e
shizhaoqun <1067928898@qq.com>
2020-10-10 17:14:18 +0800

    时照群 最大值最小值平均值

--- StudyJava/src/main/AvgClass.java
+++ StudyJava/src/main/AvgClass.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AvgClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static double avg(ArrayList arr) {
+		double sum=0;
+		for(int i=0;i<arr.size();i++)
+		{
+			sum+=(int)arr.get(i);
+		}
+		double avg=sum/arr.size();
+		return avg;
+	}
+	public static int maxGrade(ArrayList arr) {
+		int maxgrade=(int)Collections.max(arr);
+		return maxgrade;
+	}
+	public static int minGrade(ArrayList arr) {
+		int mingrade=(int)Collections.min(arr);
+		return mingrade;
 	}
 
 }
